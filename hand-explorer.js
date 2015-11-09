6c0a3f52ec33f2729181587a2ce76cd5b5bbba46
Dan Vanderkam <danvdk@gmail.com>
2015-11-09 11:01:03 -0500

    Kill orient prop

--- hand-explorer.js
+++ hand-explorer.js
@@ -9,7 +9,6 @@ var SUIT_SYMBOLS = {
  * props:
  *   suit: {'S', 'H', 'D', 'C'}
  *   rank: {'1'..'9', 'T', 'J', 'Q', 'K', 'A'}
- *   orient: {'horiz', 'vert'}
  *   facedown: {false, true}
  */
 class Card extends React.Component {
@@ -18,37 +17,36 @@ class Card extends React.Component {
     var suitSym = SUIT_SYMBOLS[suit];
     var rankSym = this.props.rank;
     if (rankSym == 'T') rankSym = '10';
-    var orient = this.props.orient;
-    var cardClass = 'card card-' + orient;
     if (this.props.facedown) {
-      cardClass += ' facedown';
-      return <div className={cardClass}><span className='rank'>{'\u00a0'}</span><span className='suit'>{'\u00a0'}</span></div>;
+      return (
+          <div className='card facedown'>
+            <span className='rank'>{'\u00a0'}</span>
+            <span className='suit'>{'\u00a0'}</span>
+          </div>
+        );
+    } else {
+      return (
+          <div className='card'>
+            <span className='rank'>{rankSym}</span>
+            <span className={'suit suit-' + suit}>{suitSym}</span>
+          </div>
+      );
     }
-    var secondSuit = null;
-    if (orient == 'vert') {
-    }
-    return (
-        <div className={cardClass}>
-          <span className='rank'>{rankSym}</span>
-          <span className={'suit small-suit suit-'+suit}>{suitSym}</span>
-          <span className={'suit big-suit suit-' + suit}>{suitSym}</span>
-        </div>
-    );
   }
 }
 
 ReactDOM.render(
   <div>
-    <Card orient='horiz' suit='S' rank='T' />
-    <Card orient='horiz' suit='H' rank='A' />
-    <Card orient='horiz' suit='C' rank='9' />
-    <Card orient='horiz' suit='D' rank='2' />
-    <Card facedown={true} orient='horiz' suit='D' rank='2' />
-    <Card orient='vert' suit='S' rank='T' />
-    <Card orient='vert' suit='H' rank='A' />
-    <Card orient='vert' suit='C' rank='9' />
-    <Card orient='vert' suit='D' rank='2' />
-    <Card facedown={true} orient='vert' suit='C' rank='9' />
+    <Card suit='S' rank='T' />
+    <Card suit='H' rank='A' />
+    <Card suit='C' rank='9' />
+    <Card suit='D' rank='2' />
+    <Card facedown={true} suit='D' rank='2' />
+    <Card suit='S' rank='T' />
+    <Card suit='H' rank='A' />
+    <Card suit='C' rank='9' />
+    <Card suit='D' rank='2' />
+    <Card facedown={true} suit='C' rank='9' />
   </div>,
   document.getElementById('root')
 );
