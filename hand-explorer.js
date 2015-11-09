7ca5d9912e61e78265e6547d4c05a48f34a93a84
Dan Vanderkam <danvdk@gmail.com>
2015-11-09 10:57:45 -0500

    attempt to mimic iBridgeBaron cards

--- hand-explorer.js
+++ hand-explorer.js
@@ -4,6 +4,14 @@ var SUIT_SYMBOLS = {
   'D': '♦',
   'C': '♣'
 };
+
+/**
+ * props:
+ *   suit: {'S', 'H', 'D', 'C'}
+ *   rank: {'1'..'9', 'T', 'J', 'Q', 'K', 'A'}
+ *   orient: {'horiz', 'vert'}
+ *   facedown: {false, true}
+ */
 class Card extends React.Component {
   render() {
     var suit = this.props.suit;
@@ -12,14 +20,18 @@ class Card extends React.Component {
     if (rankSym == 'T') rankSym = '10';
     var orient = this.props.orient;
     var cardClass = 'card card-' + orient;
-    if (this.props.hidden) {
-      cardClass += ' hidden';
+    if (this.props.facedown) {
+      cardClass += ' facedown';
       return <div className={cardClass}><span className='rank'>{'\u00a0'}</span><span className='suit'>{'\u00a0'}</span></div>;
+    }
+    var secondSuit = null;
+    if (orient == 'vert') {
     }
     return (
         <div className={cardClass}>
           <span className='rank'>{rankSym}</span>
-          <span className={'suit suit-'+suit}>{suitSym}</span>
+          <span className={'suit small-suit suit-'+suit}>{suitSym}</span>
+          <span className={'suit big-suit suit-' + suit}>{suitSym}</span>
         </div>
     );
   }
@@ -31,12 +43,12 @@ ReactDOM.render(
     <Card orient='horiz' suit='H' rank='A' />
     <Card orient='horiz' suit='C' rank='9' />
     <Card orient='horiz' suit='D' rank='2' />
-    <Card hidden={true} orient='horiz' suit='D' rank='2' />
+    <Card facedown={true} orient='horiz' suit='D' rank='2' />
     <Card orient='vert' suit='S' rank='T' />
     <Card orient='vert' suit='H' rank='A' />
     <Card orient='vert' suit='C' rank='9' />
     <Card orient='vert' suit='D' rank='2' />
-    <Card hidden={true} orient='vert' suit='C' rank='9' />
+    <Card facedown={true} orient='vert' suit='C' rank='9' />
   </div>,
   document.getElementById('root')
 );
