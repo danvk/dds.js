fc543b251c24aa35e099029b6ac34fa57bdbc93f
Dan Vanderkam <danvdk@gmail.com>
2015-11-09 22:40:06 -0500

    Parse PBN and render hand

--- hand-explorer.js
+++ hand-explorer.js
@@ -1,3 +1,55 @@
+'N:T843.K4.KT853.73 J97.J763.642.KJ5 Q52.Q982.QJ.9862 AK6.AT5.A97.AQT4'
+
+function textToRank(txt: string): number {
+  if (txt.length != 1) {
+    throw 'Invalid card symbol: ' + txt;
+  }
+  if (txt >= '2' && txt <= '9') return Number(txt);
+  if (txt == 'T') return 10;
+  if (txt == 'J') return 11;
+  if (txt == 'Q') return 12;
+  if (txt == 'K') return 13;
+  if (txt == 'A') return 14;
+  throw 'Invalid card symbol: ' + txt;
+}
+
+var SUITS = ['S', 'H', 'D', 'C'];
+
+var NEXT_PLAYER = {
+  'N': 'E',
+  'E': 'S',
+  'S': 'W',
+  'W': 'N'
+};
+
+function parsePBN(pbn: string) {
+  var parts = pbn.split(' ');
+  if (parts.length != 4) {
+    throw 'PBN must have four hands (got ' + parts.length + ')';
+  }
+
+  var m = parts[0].match(/^([NSEW]):/);
+  if (!m) {
+    throw 'PBN must start with either "N:", "S:", "E:" or "W:"';
+  }
+  var player = m[1];
+  var deal = {};
+  parts[0] = parts[0].slice(2);
+  parts.forEach((txt, i) => {
+    deal[player] = {};
+    var suits = txt.split('.');
+    if (suits.length != 4) {
+      throw `${player} must have four suits, got ${suits.length}: ${txt}`;
+    }
+    suits.forEach((holding, idx) => {
+      deal[player][SUITS[idx]] = [].map.call(holding, textToRank);
+    });
+    player = NEXT_PLAYER[player];
+  });
+  return deal;
+}
+window.parsePBN = parsePBN;
+
 var SUIT_SYMBOLS = {
   'S': '♠',
   'H': '♥',
@@ -17,6 +69,10 @@ class Card extends React.Component {
     var suitSym = SUIT_SYMBOLS[suit];
     var rankSym = this.props.rank;
     if (rankSym == 'T') rankSym = '10';
+    if (rankSym == 11) rankSym = 'J';
+    if (rankSym == 12) rankSym = 'Q';
+    if (rankSym == 13) rankSym = 'K';
+    if (rankSym == 14) rankSym = 'A';
     if (this.props.facedown) {
       return (
           <div className='card facedown'>
@@ -35,6 +91,33 @@ class Card extends React.Component {
   }
 }
 
+/**
+ * props:
+ *   hand: { 'S': [4, 9, 13], ... }
+ */
+class Hand extends React.Component {
+  render() {
+    var cards = {};
+    for (var suit in this.props.hand) {
+      var holding = this.props.hand[suit];
+      cards[suit] = holding.map(rank => <Card key={rank} suit={suit} rank={rank} />);
+    }
+    return (
+      <div className="hand">
+        {cards['S']}
+        <br/>
+        {cards['H']}
+        <br/>
+        {cards['C']}
+        <br/>
+        {cards['D']}
+      </div>
+    );
+  }
+}
+
+var hand = parsePBN('N:T843.K4.KT853.73 J97.J763.642.KJ5 Q52.Q982.QJ.9862 AK6.AT5.A97.AQT4')
+
 ReactDOM.render(
   <div>
     <Card suit='S' rank='T' />
@@ -47,6 +130,8 @@ ReactDOM.render(
     <Card suit='C' rank='9' />
     <Card suit='D' rank='2' />
     <Card facedown={true} suit='C' rank='9' />
+    <br/>
+    <Hand hand={hand['W']} />
   </div>,
   document.getElementById('root')
 );
