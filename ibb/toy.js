350b5ee8625ff285f3047d2d48082f360e61162c
Dan Vanderkam <danvdk@gmail.com>
2015-11-14 15:16:32 -0500

    Generate reference images

--- ibb/toy.js
+++ ibb/toy.js
@@ -1,19 +1,41 @@
-loadImage('ns-black.png').then(canvas => {
-  var cards = sliceImage(canvas, ibbBoxes6);
-  var sizes = {};
-  _.each(cards, (card, position) => {
-    var cardCnv = document.createElement('canvas');
-    cardCnv.width = card.width;
-    cardCnv.height = card.height;
-    cardCnv.getContext('2d').drawImage(card, 0, 0);
-    document.body.appendChild(cardCnv);
+Promise.all([
+  loadImage('ns-black.png'),
+  loadImage('ns-red.png')
+]).then(([blackImage, redImage]) => {
+  var cardsBlackNorth = sliceImage(blackImage, ibbBoxes6);
+  var cardsRedNorth = sliceImage(redImage, ibbBoxes6);
 
-    var sp = document.createElement('span');
-    sp.innerHTML = ` ${position}<br>`;
-    document.body.appendChild(sp);
+  var nsBlackSuits = {N: 'S', E: 'D', S: 'C', W: 'H'};
+  var nsRedSuits = {N: 'H', E: 'C', S: 'S', W: 'S'};
 
-    var size = cardCnv.width + 'x' + cardCnv.height;
-    sizes[size] = (sizes[size] || 0) + 1;
+  var cardsNS = [];
+  var cardsEW = [];
+
+  var recordCard = function(card, position, isNorthBlack) {
+    var player = position[0];
+    var rank = Number(position.slice(1));
+    var el;
+    if (isNorthBlack) {
+      el = {card, suit: nsBlackSuits[player], rank: 2 + rank};
+    } else {
+      el = {card, suit: nsRedSuits[player], rank: 2 + rank};
+    }
+    if (player == 'S' || player == 'N') {
+      cardsNS.push(el);
+    } else {
+      cardsEW.push(el);
+    }
+  };
+
+  _.each(cardsBlackNorth, (card, position) => {
+    recordCard(card, position, true);
   });
-  console.log(sizes);
+  _.each(cardsRedNorth, (card, position) => {
+    recordCard(card, position, false);
+  });
+
+  console.log({
+    'EW': cardsEW,
+    'NS': cardsNS
+  });
 });
